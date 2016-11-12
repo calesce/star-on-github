19dc25a1558c88d921b24873eb48c8f8afa1c387
Cale Newman <newman.cale@gmail.com>
2016-11-12 17:48:02 -0600

    star repository if GitHub URL with a repository

--- share/ShareViewController.swift
+++ share/ShareViewController.swift
@@ -10,38 +10,101 @@ import UIKit
 import Social
 import MobileCoreServices
 
-class ShareViewController: UIViewController, URLSessionTaskDelegate {
+let ACCESS_TOKEN_KEY = "accessToken"
+let APP_GROUP_NAME = "group.com.newman.cale.star"
 
+class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let context = self.extensionContext!
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
         
         for item in context.inputItems {
-            if let item = item as? NSExtensionItem,
-            let attachments = item.attachments {
-                for itemProvider in attachments {
-                    if let itemProvider = itemProvider as? NSItemProvider {
-                        itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) {
-                            (result, error) in
-                            
-                            if let url = result as? URL {
-                                let task = session.dataTask(with: url) {
-                                    (data, response, error) in
-                                    print("\(response?.url?.absoluteString)")
-                                    DispatchQueue.main.async {
-                                        self.extensionContext!.completeRequest(returningItems: nil, completionHandler: nil)
-                                    }
-                                }
-                                task.resume()
-                            } else {
-                                print("\(error)")
-                                context.completeRequest(returningItems: nil, completionHandler: nil)
-                            }
-                        } 
+            guard let item = item as? NSExtensionItem,
+            let attachments = item.attachments
+            else {
+                return self.closeExtension()
+            }
+            for itemProvider in attachments {
+                guard let itemProvider = itemProvider as? NSItemProvider
+                else {
+                    return self.closeExtension()
+                }
+                itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) {
+                    (result, error) in
+                    if let url = result as? URL {
+                        self.attemptToStarWithUrl(url: url)
+                    } else {
+                        print("\(error)")
+                        self.closeExtension()
                     }
                 }
             }
+        }
+    }
+
+    func attemptToStarWithUrl(url: URL) {
+        self.getExpandedUrl(url: url) {
+            fullUrl in
+            if let fullUrl = fullUrl,
+                let pathComponents = self.parseGitHubUrl(url: fullUrl) {
+                self.starARepository(owner: pathComponents[0], repo: pathComponents[1]) {
+                    success in
+                    if success {
+                        print("starred \(pathComponents[1])")
+                    } else {
+                        print("unable to star \(pathComponents[1])")
+                    }
+                    self.closeExtension()
+                }
+                return
+            }
+            self.closeExtension()
+        }
+    }
+
+    func getExpandedUrl(url: URL, completion: @escaping (URL?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            if let resp = response as? HTTPURLResponse {
+                return completion(resp.url)
+            }
+            return completion(nil)
+        }
+        task.resume()
+    }
+
+    func parseGitHubUrl(url: URL) -> [String]? {
+        if let host = url.host {
+            let pathComponents = url.path.characters.split{$0 == "/"}.map(String.init)
+            return (host == "github.com" && pathComponents.count > 1) ? pathComponents : nil
+        }
+        return nil
+    }
+
+    func starARepository(owner: String, repo: String, completion: @escaping (Bool) -> Void) {
+        guard let sharedDefaults = UserDefaults.init(suiteName: APP_GROUP_NAME),
+            let token = sharedDefaults.string(forKey: ACCESS_TOKEN_KEY)
+        else {
+            return completion(false)
+        }
+        let githubUrl = URL(string: "https://api.github.com/user/starred/\(owner)/\(repo)?access_token=\(token)")
+        let request = NSMutableURLRequest(url: githubUrl!)
+        request.httpMethod = "PUT"
+
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, resp, err) in
+            guard let response = resp as? HTTPURLResponse
+            else {
+                return completion(false)
+            }
+            return completion(response.statusCode == 204)
+        }
+
+        task.resume()
+    }
+
+    func closeExtension() {
+        DispatchQueue.main.async {
+            self.extensionContext!.completeRequest(returningItems: nil, completionHandler: nil)
         }
     }
 }
